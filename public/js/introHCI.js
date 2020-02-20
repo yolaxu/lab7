d255114b32c2f1be60f6cf0f21d644dbe5c85e4f
Eryue Xu <eryuexu@EryuedeMacBook-Air.local>
2020-02-20 10:57:57 -0800

    updated

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeBtn').click(clickLikeBtn);
+}
+
+function clickLikeBtn() {
+	//e.preventDefault();
+	//ga('create', 'UA-158841376-1','auto');
+	ga("send", "event", 'like', 'click');
 }
